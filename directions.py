bdc11dc70b3c6bc8f232161e43257b1a49808452
Carlos Alvarado <calvarado@cs.uchicago.edu>
2017-01-22 21:51:47 -0600

    added google examples

--- directions.py
+++ directions.py
@@ -1,23 +1,48 @@
 import googlemaps
+import json
+import requests
 from datetime import datetime
 
-key_google = 'AIzaSyBXmwexQtLS4X87d8qFf7XVFH5nnrpvAN8'
+def get_json_places(query):
+    places = requests.get(query)
+    
+    return json.loads(places.text)
 
-gmaps = googlemaps.Client(key= key_google)
+def get_gmaps():
+    gmaps = googlemaps.Client(key= key_google)
 
-# Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-print(geocode_result)
+    # Geocoding an address
+    geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+    print(geocode_result)
 
-# Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-print(reverse_geocode_result)
+    # Look up an address with reverse geocoding
+    reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+    print(reverse_geocode_result)
 
-# Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="transit",
-                                     departure_time=now)
+    # Request directions via public transit
+    now = datetime.now()
+    directions_result = gmaps.directions("Sydney Town Hall",
+                                         "Parramatta, NSW",
+                                         mode="transit",
+                                         departure_time=now)
 
-print(directions_result)
+    print(directions_result)
+
+
+if __name__ == '__main__':
+
+    key_google = 'AIzaSyBXmwexQtLS4X87d8qFf7XVFH5nnrpvAN8'
+
+    query = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key={}'.format(key_google)
+
+    places = get_json_places(query)
+
+    for niter, p in enumerate(places['results']):
+        print('Place {}'.format(niter))
+        print(p)
+
+
+
+    #get_gmaps()
+
+    
