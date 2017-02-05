fac82cf8bc3abcfcd5105efacc0db0fadf3efb64
lnoel <lnoel@uchicago.edu>
2017-02-04 19:23:35 -0600

    developed route_optimization

--- route_optomization.py
+++ route_optomization.py
@@ -4,14 +4,14 @@ from query_google import helper_transit_time
 
 
 #Example data structure for this file:
-places_dict = {'ChIJp2E8Hb8sDogR8WK83agX6c4': (41.8789, -87.6359, 2.79, time_open, time_close), # willis tower
-            'ChIJB5o6CWvTD4gR25QC-QbMQAk': (41.9211, -87.6340, 3.56, time_open, time_close), #lincoln park zoo
-            'ChIJHd8BYAopDogRBuMXc6oszA8': (41.7923, -87.5804, 3.98, time_open, time_close), #MSI
-            'ChIJ9ZKOJxQsDogRuJBKj2ZPhi8': (41.8299, -87.6338, 4.07, time_open, time_close), #White Sox Field
-            'ChIJId-a5bLTD4gRRtbdduE-6hw': (41.9484, -87.6553, 4.93, time_open, time_close)} #Wrigley Field
-LAT = 0
-LON = 1
-PRIORITY = 2
+#Format of each dict entry- id: lon, lat, rating, open_time, close_time, avg_time, temp_dummy for testing
+places_dict = {'ChIJp2E8Hb8sDogR8WK83agX6c4': (41.8789, -87.6359, 2.79, 800, 1500,10, 20), # willis tower
+            'ChIJB5o6CWvTD4gR25QC-QbMQAk': (41.9211, -87.6340, 3.56, 900, 1900, 90, 10), #lincoln park zoo
+            'ChIJHd8BYAopDogRBuMXc6oszA8': (41.7923, -87.5804, 3.98, 730, 2000, 75, 14), #MSI
+            'ChIJ9ZKOJxQsDogRuJBKj2ZPhi8': (41.8299, -87.6338, 4.07, 1300, 2330, 135, 13), #White Sox Field
+            'ChIJId-a5bLTD4gRRtbdduE-6hw': (41.9484, -87.6553, 4.93, 1145, 1745, 80, 31)} #Wrigley Field
+
+LAT, LON, PRIORITY, OPEN, CLOSE, DURATION, TEMP_TIME = 0, 1, 2, 3, 4, 5, 6
 
 
 def haversine(lon1, lat1, lon2, lat2):
@@ -53,27 +53,24 @@ def permutations(p):
     return rv
 
 
-def prelim_sort(places, running_order, accuracy_degree = 3):
+def prelim_geo_sort(places_dict, running_order, accuracy_degree = 2):
     '''
     Provides a preliminary ranking of node routes based on geographic distance.
     Inputs:
         places: dict
-        labels: list of string
+        labels: list of list of strings
         accuracy_degree: int (optional, deefaults to 3) 
     '''
-
-    #running_order = permutations(labels)
     rv = []
     for element in running_order:
-        print(element)
         running_distance = 0
         for i in range(len(element) - 1):
             id_0 = element[i]
             id_1 = element[i + 1]
-            lon_0 = places[id_0][LON]
-            lat_0 = places[id_0][LAT]
-            lon_1 = places[id_1][LON]
-            lat_1 = places[id_1][LAT]
+            lon_0 = places_dict[id_0][LON]
+            lat_0 = places_dict[id_0][LAT]
+            lon_1 = places_dict[id_1][LON]
+            lat_1 = places_dict[id_1][LAT]
             distance = haversine(lon_0, lat_0 , lon_1, lat_1)
             running_distance += distance
         rv.append((distance, element))
@@ -81,65 +78,89 @@ def prelim_sort(places, running_order, accuracy_degree = 3):
     rv = [i[1] for i in rv]
     return rv[:accuracy_degree]
 
-def get_min_cost(ordered_routes, delimiter = None):
+
+def get_min_cost(ordered_routes, begin_time, end_time, num_included_places):
     '''
     Calculates the minimum cost to pass through each node in a set
     in any order.
     Input:
-        times: dict
-        delimiter: integer (optional)
+        ordered_routes: dict
+        begin_time: integer
+        end_time: integer
     Return: list of strings, integer
     '''
-    
-    if delimiter:
-        ordered_routes = ordered_routes[:delimiter + 1]
+
     optimal = None
-    best_cost = 99999
-    
+    best_time = 999999
+    failed_all_open = []
+
     for choice in ordered_routes:
-        cost = 0
+        choice = choice[:num_included_places + 1]
+        all_open = True
+        time = begin_time
         node = choice[:]
-        while len(node) > 1:
+        priority_score = 0
+        while len(node) > 2:
             begin = node[0]
             end = node[1]
-            #implement call to get transit times here
-            cost += helper_transit_time(begin, end)
+            #If location isn't open at begining or end of projected time, this route has
+            #second class status. Builds priority score, a measure of how many desirable
+            #sites are open in this route, based on location ranking.
+            if places_dict[begin][OPEN] > time:
+                all_open = False
+            else:
+                priority_score += .5 * places_dict[begin][PRIORITY]
+            time += places_dict[begin][DURATION]
+            if places_dict[begin][CLOSE] < time:
+                all_open = False
+            else:
+                priority_score += .5 * places_dict[begin][PRIORITY]
+            #add parameters w/ respect to current time, day 
+            time += helper_transit_time(begin, end)
+            #time += places_dict[begin][TEMP_TIME] #temp, to avoid using google API too much
             del node[0]
 
-        if cost < best_cost:
+
+        if time < best_time and all_open:
             optimal = choice
-            best_cost = cost
-         
-    return optimal, best_cost
+            best_time = time
+        else:
+            failed_all_open.append((priority_score, choice, time))
+      
+    #If there is at least one route with all locations open, return route 
+    #with best time. Else return route with top priority score.
+    if optimal != None:
+        return optimal, time, None
+    else:
+        failed_all_open = sorted(failed_all_open, key = lambda x: (x[0], x[2]))
+        #print(failed_all_open[0])
+        return failed_all_open[0][1], failed_all_open[0][2], failed_all_open[0][0]
+        #return failed_all_open[0][1], failed_all_open[0][2], failed_all_open[0][0]
 
 
-def optomize(places, max_cost, return_delimiter = False):
+def optomize(places_dict, begin_time, end_time, date = None):
     '''
     Determines how many nodes can be visited given upper cost
     constraint.
     Inputs:
-        places: dict
-        max_time: int
-        return_delimiter: bool
+        places_dict: dict
+        begin_time: int
+        end_time: int
+        date: string
     Returns: list of strings, integer, (integer)
     '''
-    labels = list(places.keys())
+    labels = list(places_dict.keys())
     running_order = permutations(labels)
-    places = prelim_sort(places, running_order)
-    time, delimiter, route = 0, 3, []
-    num_nodes = len(places)
-    while time <= max_cost and delimiter <= num_nodes:
-        prev_route, prev_time = route, time
-        route, time = get_min_cost(places, delimiter)
-        delimiter += 1
+    updated_places = prelim_geo_sort(places_dict, running_order)
+    route = []
+    time = 99999999
+    num_included_places = len(labels)
+    while time > end_time and num_included_places > 3:
+        route, time, priority_score = get_min_cost(updated_places, begin_time, end_time, num_included_places)
+        num_included_places -= 1
     
-    if prev_route == []: return False
-    delimiter -= 1
-    if return_delimiter:
-        rv = prev_route, prev_time, delimiter
-    else:
-        rv = prev_route, prev_time
-    return rv
+
+    return route, time
 
 if __name__ == '__main__':
     
